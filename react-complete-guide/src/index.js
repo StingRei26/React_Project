686c9cf145fd5886ff4954c2ef8b069c56347e55
Reiniel Ravelo <reinielravelo@gmail.com>
2019-06-20 19:51:23 -0400

    components

--- react-complete-guide/src/index.js
+++ react-complete-guide/src/index.js
@@ -4,5 +4,7 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+// This is he main component, reads the code inside the App.js folder 
+// and inputs it inside the root div that lives in the public html file which is what renders to the page
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
