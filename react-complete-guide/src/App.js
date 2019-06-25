f21801f98d93b6f3c97a45691e3117359937bd11
Reiniel Ravelo <reinielravelo@gmail.com>
2019-06-24 20:07:08 -0400

    update

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -3,14 +3,39 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  
+  state = {
+    persons : [
+      {name: 'Max', age: 28},
+      {name: 'Reiniel', age: 29},
+      {name: 'Mia', age: 29},
+    ]
+  }
+
+  switchNameHandler = () => {
+    console.log("this was clicked"); 
+  }
+  
   render() {
     return (
       <div className="App">
        <h1> Hi my name is reiniel </h1>
        <p> This is an example of creating a second element </p>
-       <Person />
+       
+       
+       <button onClick={this.switchNameHandler}>Switch Name</button>
+       
+       
+       {/*Using State*/}
+       <Person name = {this.state.persons[0].name} age={this.state.persons[0].age} />
+       <Person name = {this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Racing</Person>
+       <Person name = {this.state.persons[2].name} age={this.state.persons[2].age} /> 
       </div>
     );
+       {/*<Person name = "Max" age="28" />
+       <Person name = "Reiniel" age="29">My Hobbies: Racing</Person>
+       <Person name = "Mia" age ="23" />*/}
+
   }
 }
 
